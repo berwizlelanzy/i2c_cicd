578d32620c90d48d5faeeb0155492c66a71693ce
berwizlelanzy <63525782+berwizlelanzy@users.noreply.github.com>
2024-10-23 16:43:56 +0700

    Update haerin.blade.php

--- resources/views/haerin.blade.php
+++ resources/views/haerin.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Minjeeey</title>
+    <title>Kang Kittyz</title>
 </head>
 <body>
     <img src="https://rilis.id/image/2023/12/16kHrptCa_96ED8DC1-9386-4290-BF91-54FBCF2F19C8.jpeg" style="display: block; margin: auto; width: 100vh;">
