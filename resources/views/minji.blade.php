c497e1ab03914e91df121d8166b3ca306ef85774
berwizlelanzy <63525782+berwizlelanzy@users.noreply.github.com>
2024-10-23 16:24:00 +0700

    Minjeyyy

---
+++ resources/views/minji.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Minjeeey</title>
+</head>
+<body>
+    <img src="https://m.media-amazon.com/images/I/61PPxM-HcdL._AC_UF1000,1000_QL80_.jpg" style="display: block; margin: auto; width: 100vh;">
+</body>
+</html>
